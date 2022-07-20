4fcc47bb2c35a45af2307199c25be5961f4aa39a
er-priyanka <shahanepriyankas01@gmail.com>
2022-07-20 16:29:38 +0530

    added bottom part inside of home page and added functionality part of FAQs part

---
+++ jsFiles/index.js
@@ -0,0 +1,33 @@
+let queAngles = document.querySelectorAll(".queAngle");
+let answers = document.querySelectorAll(".ans");
+
+let makeDefaultAngle = ()=>{
+    queAngles.forEach((e, i)=>{
+        e.classList.add("fa-angle-down");
+        e.classList.remove("fa-angle-up");
+        answers[i].style.display = "none";
+    })
+} 
+
+
+queAngles.forEach((e, i)=>{
+    
+    e.style.cursor = 'pointer';
+    let clicks = 0;
+    e.addEventListener("click", ()=>{
+        clicks++;
+        makeDefaultAngle();
+        
+        if(e.classList[2] =="fa-angle-down" && clicks%2==1){
+            e.classList.add("fa-angle-up");
+            e.classList.remove("fa-angle-down");
+            answers[i].style.display = "contents";
+        }
+        else{
+            e.classList.add("fa-angle-down");
+            e.classList.remove("fa-angle-up");
+            answers[i].style.display = "none";
+        }
+    })
+})
+
